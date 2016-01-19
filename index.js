c4d7f583ac2603482cd3cf7be001b4da29b2015e
Roi <roisagiv@Rois-MacBook-Pro.local>
2016-01-19 07:11:20 +0200

    added knex

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var express = require('express');
+var pg = require('pg');
+var knexfile = require('./knexfile');
+var knex = require('knex')(knexfile);
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -6,6 +10,14 @@ app.set('port', (process.env.PORT || 5000));
 app.get('/', function(request, response) {
   console.log('GET /');
   response.send('hello world\n');
+});
+
+app.get('/db', function (request, response) {
+  // knex_migrations
+  knex.withSchema('pg_catalog').select('*').from('pg_tables').then(function (rows){
+    console.log(rows);
+    response.send('knex!\n');
+  });
 });
 
 app.listen(app.get('port'), function() {
