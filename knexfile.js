af2b2a8d0ee4bf1dfd54f81eba2bf87bf63c62de
Roi <roisagiv@Rois-MacBook-Pro.local>
2016-01-20 14:57:06 +0200

    knex changes

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,26 @@
 // Update with your config settings.
 
 module.exports = {
-  client: 'postgresql',
-  connection: process.env.DATABASE_URL,
-  pool: {
-    min: 2,
-    max: 10
+  development: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   },
-  migrations: {
-    tableName: 'knex_migrations'
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   }
 };
